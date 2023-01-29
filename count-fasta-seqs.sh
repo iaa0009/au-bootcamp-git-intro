f0e67cefae3507a9fd2b1c664add4736f8996d1e
iaa0009 <iaa0009@auburn.edu>
2023-01-29 16:54:17 -0600

    This is Exercise_1 script
    This script should run over different fasta files and reflects the number of sequences in as well as the name of each file.
    I was not successful in writing a code to reflect the total number of sequences across all files.
    
    This is my 1st trial

--- count-fasta-seqs.sh
+++ count-fasta-seqs.sh
@@ -63,7 +63,8 @@
 # files that were 'given to' this script. The variable "$@" will be very useful
 # for this. Let's take a look at what it gives us:
 
-echo "$@"
+
+
 
 # How are you going to work with each file path?
 # HINT: for loop (remember "for do done"?)
@@ -93,4 +94,18 @@ echo "$@"
 # Good luck!
 #
 # ADD YOUR CODE BELOW:
+
+for file in "$@"
+
+ do
+
+#To count the number of sequences in each file
+   Seq1=`grep -c ">" $file`  
+   Name=`basename $file`   
+#To write the output in each file
+  echo $Seq1 $Name
+
+done
+      expr `grep -c ">" $file`        
+
 
